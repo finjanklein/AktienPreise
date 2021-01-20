315bdc3ebc4f6aa3805dedbcfe2cdf42da1de335
finjanklein <jannes456@gmail.com>
2021-01-20 19:23:38 +0100

    Erster Commit:

---
+++ src/main/java/com/example/AktienPreise/TestClass.java
@@ -0,0 +1,5 @@
+package com.example.AktienPreise;
+
+public class TestClass {
+
+}
---
+++ src/test/java/com/example/AktienPreise/AktienPreiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AktienPreise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AktienPreiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
